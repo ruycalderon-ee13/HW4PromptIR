0628cae1b1efc0cc4c9058d42493f1a888cbc052
Ruy@DustyCanteen <dustycanteen@gmail.com>
2026-05-29 18:23:44 +0800

    increase patch size

--- options.py
+++ options.py
@@ -6,13 +6,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--cuda', type=int, default=0)
 
 parser.add_argument('--epochs', type=int, default=120, help='maximum number of epochs to train the total model.')
-parser.add_argument('--batch_size', type=int,default=120,help="Batch size to use per GPU")
+parser.add_argument('--batch_size', type=int,default=80,help="Batch size to use per GPU")
 parser.add_argument('--lr', type=float, default=2e-4, help='learning rate of encoder.')
 
 parser.add_argument('--de_type', nargs='+', default=['derain', 'desnow'],
                     help='which type of degradations is training and testing for.')
 
-parser.add_argument('--patch_size', type=int, default=64, help='patchsize of input.')
+parser.add_argument('--patch_size', type=int, default=96, help='patchsize of input.')
 parser.add_argument('--num_workers', type=int, default=8, help='number of workers.')
 
 # path
